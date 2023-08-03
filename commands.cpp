d5ee1afb71971869da92e948616c1b058c086609
OsipovDN <didison88@gmail.com>
2023-08-03 14:09:50 +0300

    Шаблон для решения основного условия

--- commands.cpp
+++ commands.cpp
@@ -3,15 +3,33 @@
 #include <algorithm>
 #include <vector>
 
+using PulBlock = std::vector<std::string>;
+
 bool isDig(char* arg) {
 	std::string num = arg;
 	auto isdig = [](char ch) {return std::isdigit(ch); };
 	return std::all_of(num.cbegin(), num.cend(), isdig);
 }
 
+void addBlock(PulBlock& p, const size_t& c) {
+	size_t i = 0;
+	std::string block;
+	while (i != c) {
+		std::cin >> block;
+		if (block == "EOF") {
+			p.push_back(block);
+			break;
+		}
+		else
+			p.push_back(block);
+		i++;
+	}
+}
+
 int main(int argc, char* argv[])
 {
 	int block_count;
+	//Проверка корректности ввода
 	if (argc != 2) {
 		std::cout << "Incorrect argument input" << std::endl;
 		exit(1);
@@ -20,20 +38,31 @@ int main(int argc, char* argv[])
 		std::cout << "Not a val" << std::endl;
 		exit(1);
 	}
+	//Фиксируем введенное значение
 	block_count = atoi(argv[1]);
 
-	std::vector<std::string> pul_block;
-	std::string block;
-	size_t i = 0;
-	while (i != block_count) {
-		std::cin >> block;
-		pul_block.push_back(block);
-		i++;
-	}
+	//Заполнение статического блока
+	PulBlock pul_block;
+	bool flag = false;
+	auto print = [](const std::string& bl) {
+		if (bl != "EOF")
+			std::cout << bl << " ";
+	};
 
-	auto predPrint = [](const std::string& bl) {
-		std::cout << bl << std::endl; };
-	std::for_each(pul_block.cbegin(), pul_block.cend(), predPrint);
+	do {
+		addBlock(pul_block, block_count);
+		std::vector< std::string >::const_iterator iter = pul_block.begin();
+		flag = std::any_of(iter, pul_block.cend(), [](const std::string& com) {return com == "EOF"; });
+		std::cout << "bulk: ";
+		std::for_each(pul_block.cbegin(), pul_block.cend(), print);
+		std::cout << std::endl;
+		iter += block_count;
+
+
+	} while (!flag);
+
+
+
 
 	return 0;
 }
