87850a0a560d37a3a84756364ba1b01a7f1f94b9
OsipovDN <didison88@gmail.com>
2023-09-07 17:00:25 +0300

    Проработка команд

--- bulk.cpp
+++ bulk.cpp
@@ -6,51 +6,43 @@
 using StaticPullBlock = std::vector<std::string>;
 using DynamicPullBlock = std::deque<std::string>;
 
-struct Cmd {
-private:
-	std::time_t time;
-	std::string name_cmd;
-public:
-	Cmd(std::string& com) :
-		name_cmd(com),
-		time(std::chrono::system_clock::to_time_t(std::chrono::system_clock::now())) {}
-	std::string getName() { return name_cmd; }
-	std::time_t getTime() { return time; }
-};
-
 
 class Receiver {
 private:
 	StaticPullBlock st_pl_cmd;
 	DynamicPullBlock dn_pl_cmd;
 	size_t scope_block;
-	bool is_open;
 
-	bool isScope(const std::string& str) {
-		if (str == "{") {
-			is_open = !is_open;
-			scope_block++;
-			return true;
-		}
-		else if (str == "}") {
-			is_open = !is_open;
-			scope_block--;
-			return true;
-		}
-		else
-			return false;
-
-	}
 	std::string getNameFile() {
 		std::time_t time = std::chrono::system_clock::to_time_t(std::chrono::system_clock::now());
 		std::string name = "bulk" + std::to_string(time) + ".log";
 		return name;
 	}
+	void addStBlock(const std::string& str) {
+		st_pl_cmd.emplace_back(str);
+	}
+
+	void addDynBlock(const std::string& str) {
+		dn_pl_cmd.emplace_back(str);
+	}
 
 public:
-	explicit Receiver(int& count) :scope_block(0), is_open(false) {
+	explicit Receiver(int& count) :scope_block(0) {
 		st_pl_cmd.reserve(count);
 	}
+
+	void openBlock() {scope_block++;}
+	void closeBlock() {scope_block--;}
+	bool isFull() { return st_pl_cmd.size() == st_pl_cmd.capacity(); }
+	void addBlock(const std::string& cmd) {
+		DynamicPullBlock temp;
+		if (scope_block == 0 && dn_pl_cmd.size() == 0) {
+			addStBlock(cmd);
+		}
+		else
+			addDynBlock(cmd);
+	}
+
 
 	template <typename T>
 	bool saveBlock(T obj) {
@@ -79,75 +71,106 @@ public:
 class Command {
 protected:
 	Receiver* res;
-	Command(Receiver& r) :res(r) {};
+	Command(Receiver* r) :res(r) {};
 public:
-	virtual void execute() const = 0;
+	virtual void execute(const std::string& cmd) = 0;
 	virtual ~Command() {};
 
 };
 
+class ReadFromConsol :public Command {
+private:
+	bool isScope(const std::string& s) {
+		bool flag = false;
+		if (s == "{") {
+			res->openBlock();
+			flag = true;
+		}
+		if (s == "}") {
+			res->closeBlock();
+			flag = true;
+		}
+		return flag;
+	}
+public:
+	explicit ReadFromConsol(Receiver* r) :Command(r) {}
+	void execute(const std::string& str) override {
+		if (!isScope(str))
+			res->addBlock(str);
+	}
+	
+};
+
 class WriteToConsol :public Command {
 public:
-	explicit WriteToConsol(Receiver& r) :Command(r) {};
-	void execute() const override {};
+	explicit WriteToConsol(Receiver* r) :Command(r) {};
+	void execute(const std::string& cmd) override {
+		res->printBlockToStream(std::cout);
+	
+	};
 };
 
 class WriteToFile :public Command {
 public:
-	explicit WriteToFile(Receiver& r) :Command(r) {};
-	void execute() const override {};
+	explicit WriteToFile(Receiver* r) :Command(r) {};
+	void execute(const std::string&) override {};
 };
 
-//class ReadFromConsol :public Command {
-//	explicit ReadFromConsol(std::string& cmd) {}
-//	void execute() const override {}
-//	void executeUnDo() override {}
-//};
+
 
 
 class Invoker {
+private:
+	std::vector<Command*> commands;
 public:
-	Invoker(Command* com) {
-		com->execute();
+	void setCommands(Command* com) {
+		commands.emplace_back(com);
 	};
+
+	void runCommands(std::string& com) {
+		for (const auto cmd : commands) {
+			cmd->execute(com);
+		}
+	}
 };
 
-	class Client {
-	private:
-		Receiver* res;
 
-	public:
-		explicit Client(Receiver* r) :res(r) {}
-		void run(std::string& cmd) {
-		}
-	};
+bool isDig(char* arg) {
+	std::string num = arg;
+	auto isdig = [](char ch) {return std::isdigit(ch); };
+	return std::all_of(num.cbegin(), num.cend(), isdig);
+}
 
+int main(int argc, char* argv[])
+{
 
-	bool isDig(char* arg) {
-		std::string num = arg;
-		auto isdig = [](char ch) {return std::isdigit(ch); };
-		return std::all_of(num.cbegin(), num.cend(), isdig);
+	if (argc != 2) {
+		std::cout << "Incorrect argument input" << std::endl;
+		exit(1);
+	}
+	if (!isDig(argv[1])) {
+		std::cout << "Not a val" << std::endl;
+		exit(1);
 	}
 
-	int main(int argc, char* argv[])
-	{
+	int count = atoi(argv[1]);
+	/*ComandDistr CmdDis(count);
+	CmdDis.run();*/
 
-		if (argc != 2) {
-			std::cout << "Incorrect argument input" << std::endl;
-			exit(1);
-		}
-		if (!isDig(argv[1])) {
-			std::cout << "Not a val" << std::endl;
-			exit(1);
-		}
+	Receiver* res = new Receiver(count);
+	Invoker* inv = new Invoker();
+	inv->setCommands(new ReadFromConsol(res));
+	inv->setCommands(new WriteToConsol(res));
+	inv->setCommands(new WriteToFile(res));
+	std::string cmd;
 
-		int count = atoi(argv[1]);
-		/*ComandDistr CmdDis(count);
-		CmdDis.run();*/
-
-		Receiver* res = new Receiver(count);
-		Invoker* inv = new Invoker();
-
-
-		return 0;
+	while (std::getline(std::cin, cmd)) {
+		inv->runCommands(cmd);
 	}
+
+	delete inv;
+	delete res;
+	
+
+	return 0;
+}
